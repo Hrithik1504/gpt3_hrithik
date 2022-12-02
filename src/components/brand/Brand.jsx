07403d01b2e2edb1f5c0db195628c1315695ca18
Hrithik <warm9087@gmail.com>
2022-12-02 14:36:37 +0530

    brand implementation

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
 import './brand.css';
+import { google, slack, atlassian, dropbox, shopify } from '../brand/import';
+
 function Brand() {
   return (
     <div>
-      Brand
+      <div className="gpt3__brand section__padding">
+        <div>
+          <img src={google} alt="google" />
+        </div>
+        <div>
+          <img src={slack} alt="slack" />
+        </div>
+        <div>
+          <img src={atlassian} alt="atlassian" />
+        </div>
+        <div>
+          <img src={dropbox} alt="google" />
+        </div>
+        <div>
+          <img src={shopify} alt="shopify" />
+        </div>
+      </div>
     </div>
   )
 }
