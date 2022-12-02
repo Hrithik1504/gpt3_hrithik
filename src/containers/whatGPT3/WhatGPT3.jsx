505f00c46f9631f75fc6b9ca015764a74a92929e
Hrithik <warm9087@gmail.com>
2022-12-02 15:13:44 +0530

    features implementation done

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
-import './whatgpt3.css'
+import React from 'react';
+import Feature from '../features/Features';
+import './whatgpt3.css';
+
+
 function WhatGPT3() {
   return (
-    <div>
-      WhatGPT3
+    <div className="gpt3__whatgpt3 section__margin" id="whpt3">
+      <div className="gpt3__whatgpt3-feature">
+        <Feature title="What is GPT3" text="We so opinion friends me message as delight. Whole front do of plate head oh ought." />
+      </div>
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className="gradient__text">The Possibilites are beyond of Imagination.</h1>
+        <p>Explore the Library</p>
+      </div>
+
+      <div className="gpt3__whatgpt3-container">
+        <Feature title="Chat Box" text="We so opinion friends me message as delight. Whole fron do of plate hear oh ought." />
+        <Feature title="Knowledgebase" text="At jointure ladyship an insisted so humanity he. Friendly bechelor entrence to on by. As put impossible own apartments be."/>
+        <Feature title="Education" text="At jointure ladyship an insisted so humanity he. Friendly bechelor entrence to on by. As put impossible own apartments be."/>
+      </div>
     </div>
   )
 }
